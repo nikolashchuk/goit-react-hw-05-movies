e38efff370d73aaa034180d353e26efecb3613db
nikolashchuk <nikolashchuk@gmail.com>
2023-06-26 21:08:00 +0300

    update

--- src/components/CastMarkup/CastMarkup.jsx
+++ src/components/CastMarkup/CastMarkup.jsx
@@ -2,22 +2,30 @@ import PropTypes from 'prop-types';
 import { CastList } from './CastMarkupStyled';
 
 export default function CastMarkup({ cast }) {
+  const GOOGLE_SERACH = 'https://www.google.com/search?q=';
+
   return (
     <CastList>
       {cast &&
         cast.map(({ id, name, profile_path, character }) => (
-          <li key={id}>
-            <img
-              src={
-                profile_path &&
-                `https://image.tmdb.org/t/p/original${profile_path}`
-              }
-              alt={name}
-              height="300"
-            />
-            <h2>{name}</h2>
-            <p>Character: {character}</p>
-          </li>
+          <a
+            href={GOOGLE_SERACH + name}
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <li key={id}>
+              <img
+                src={
+                  profile_path &&
+                  `https://image.tmdb.org/t/p/original${profile_path}`
+                }
+                alt={name}
+                height="300"
+              />
+              <h2>{name}</h2>
+              <p>Character: {character}</p>
+            </li>
+          </a>
         ))}
     </CastList>
   );
